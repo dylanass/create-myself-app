6770e31849bfc9b78e2eabb600ce47a8726645a0
Dylan <ecarx@E01M210081.local>
2021-08-31 11:40:31 +0800

    feat(dev): 修改JSDoc注释 TypeScript

--- src/components/hooks/useClientRect.ts
+++ src/components/hooks/useClientRect.ts
@@ -1,22 +1,25 @@
 import React, { useState, useCallback } from "React";
 
-interface RectItem {
-  bottom: number;
-  height: number;
-  left: number;
-  right: number;
-  top: number;
-  width: number;
-  x: number;
-  y: number;
-}
-interface Item {
-  ref: () => void;
-  rect: RectItem;
-}
 /**
- * @description 测量 DOM 节点 
- * @returns {Item[]}
+ * @typedef {Object} RectItem
+ * @property {number} bottom
+ * @property {number} height
+ * @property {number} left
+ * @property {number} right
+ * @property {number} top
+ * @property {number} width
+ * @property {number} x
+ * @property {number} y
+ */
+/**
+ * @typedef {Object} Item
+ * @property {()=>void} ref
+ * @property {RectItem} rect
+ */
+
+/**
+ * @description 测量 DOM 节点
+ * @returns {[...Item]}
  */
 function useClientRect() {
   const [rect, setRect] = useState(null);

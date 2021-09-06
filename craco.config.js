258abe0544eb2198341f1e477a1ce31e1d55bce0
Dylan <ecarx@E01M210081.local>
2021-09-06 16:35:20 +0800

    feat(dev): tailwindcss

--- craco.config.js
+++ craco.config.js
@@ -8,4 +8,10 @@ module.exports = {
       "@": resolveToStaticPath("./src"),
     },
   },
+
+  style: {
+    postcss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
+  },
 };

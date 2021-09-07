71edacf4f9fac776dc66dcbbdc69713e2e12d458
Dylan <ecarx@E01M210081.local>
2021-09-07 19:03:34 +0800

    feat(dev): 404 login 页面重定向配置

--- src/constants/routes-config.tsx
+++ src/constants/routes-config.tsx
@@ -3,50 +3,54 @@ import NotFoundPage from "@/view/not-found-page/NotFoundPage";
 import { Redirect } from "react-router-dom";
 
 const routes = [
-  // {
-  //   path: "/basic",
-  //   exact: true,
-  //   component: <LazyLoad component={() => import("@/view/basic")} />,
-  //   children: [
-  //     {
-  //       path: "/home/composition",
-  //       component: Composition,
-  //     },
-  //     {
-  //       path: "/home/flows",
-  //       component: Flows,
-  //       auth: true,
-  //     },
-  //     {
-  //       path: "/home/user",
-  //       component: User,
-  //       auth: true,
-  //     },
-  //     {
-  //       path: "/home/source",
-  //       component: Source,
-  //       auth: true,
-  //     },
-  //     {
-  //       path: "/home",
-  //       redirect: "/home/composition",
-  //     },
-  //   ],
-  // },
+  {
+    path: "/basic",
+    exact: true,
+    component: () => <LazyLoad component={() => import("@/view/basic")} />,
+    // children: [
+    //   {
+    //     path: "/home/composition",
+    //     component: Composition,
+    //   },
+    //   {
+    //     path: "/home/flows",
+    //     component: Flows,
+    //     auth: true,
+    //   },
+    //   {
+    //     path: "/home/user",
+    //     component: User,
+    //     auth: true,
+    //   },
+    //   {
+    //     path: "/home/source",
+    //     component: Source,
+    //     auth: true,
+    //   },
+    //   {
+    //     path: "/home",
+    //     redirect: "/home/composition",
+    //   },
+    // ],
+  },
   {
     path: "/login",
     exact: true,
     component: () => <LazyLoad component={() => import("@/view/login")} />,
   },
-  // {
-  //   path: "/",
-  //   exact: true,
-  //   component: <Redirect to="/login" />,
-  // },
-  // {
-  //   //没有path 导入404页面
-  //   component: <NotFoundPage />,
-  // },
+  {
+    path: "/",
+    exact: true,
+    component: () => <Redirect to="/login" />,
+  },
+  {
+    //没有path 导入404页面
+    path: "/404",
+    component: () => <NotFoundPage />,
+  },
+  {
+    component: () => <Redirect to="/404" />,
+  },
 ];
 
 export default routes;

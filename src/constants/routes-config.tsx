9a374df0923547b7626ea9db77c62d89c8c0fabe
Dylan <ecarx@E01M210081.local>
2021-09-07 18:54:59 +0800

    feat(dev): 改变component暴露方式

--- src/constants/routes-config.tsx
+++ src/constants/routes-config.tsx
@@ -1,47 +1,52 @@
 import LazyLoad from "@/components/LazyLoad";
+import NotFoundPage from "@/view/not-found-page/NotFoundPage";
+import { Redirect } from "react-router-dom";
 
 const routes = [
-  {
-    path: "/basic",
-    component: <LazyLoad component={() => import("@/view/basic")} />,
-    // children: [
-    //   {
-    //     path: "/home/composition",
-    //     component: Composition,
-    //   },
-    //   {
-    //     path: "/home/flows",
-    //     component: Flows,
-    //     auth: true,
-    //   },
-    //   {
-    //     path: "/home/user",
-    //     component: User,
-    //     auth: true,
-    //   },
-    //   {
-    //     path: "/home/source",
-    //     component: Source,
-    //     auth: true,
-    //   },
-    //   {
-    //     path: "/home",
-    //     redirect: "/home/composition",
-    //   },
-    // ],
-  },
+  // {
+  //   path: "/basic",
+  //   exact: true,
+  //   component: <LazyLoad component={() => import("@/view/basic")} />,
+  //   children: [
+  //     {
+  //       path: "/home/composition",
+  //       component: Composition,
+  //     },
+  //     {
+  //       path: "/home/flows",
+  //       component: Flows,
+  //       auth: true,
+  //     },
+  //     {
+  //       path: "/home/user",
+  //       component: User,
+  //       auth: true,
+  //     },
+  //     {
+  //       path: "/home/source",
+  //       component: Source,
+  //       auth: true,
+  //     },
+  //     {
+  //       path: "/home",
+  //       redirect: "/home/composition",
+  //     },
+  //   ],
+  // },
   {
     path: "/login",
-    component: <LazyLoad component={() => import("@/view/login")} />,
+    exact: true,
+    component: () => <LazyLoad component={() => import("@/view/login")} />,
   },
-  {
-    path: "/",
-    redirect: "/login",
-  },
-  {
-    //没有path 导入404页面
-    component: <LazyLoad component={() => import("@/view/not-found-page/NotFoundPage")} />,
-  },
+  // {
+  //   path: "/",
+  //   exact: true,
+  //   component: <Redirect to="/login" />,
+  // },
+  // {
+  //   //没有path 导入404页面
+  //   component: <NotFoundPage />,
+  // },
 ];
 
 export default routes;
